b1b6c2637386f9adc4978a58afc524618486ea5b
shark300 <attila.hajdu@vodafone.com>
2022-05-10 13:58:33 +0200

    feat: extraction from extractors should be optional and fix documentation issues

--- tasks/extractor/tests/integration/extractor.test.ts
+++ tasks/extractor/tests/integration/extractor.test.ts
@@ -19,6 +19,14 @@ describe("Extract Jira keys", function () {
     }
   }
 
+  beforeEach(() => {
+    process.env["SYSTEM_TEAMPROJECT"] = "myproject";
+    process.env["BUILD_BUILDID"] = "1";
+    process.env["SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"] =
+      "https://my-azure-server/myorganization/";
+    process.env["SYSTEM_ACCESSTOKEN"] = "token";
+  });
+
   it("should succeed and publish environment variable with collected jira keys", function (done: Mocha.Done) {
     // given
     this.timeout(5000);
@@ -34,6 +42,27 @@ describe("Extract Jira keys", function () {
     expect(tr.warningIssues.length).to.equals(0, "should have no warnings");
     expect(tr.errorIssues.length).to.equals(0, "should have no errors");
     expect(getVariable(tr, "Jira.Keys")).to.equal("JIE-541, JIE-1257");
+    console.log(tr.stdout);
+    done();
+  });
+
+  it("should succeed and publish environment variable with collected jira keys without matching pull request", function (done: Mocha.Done) {
+    // given
+    this.timeout(5000);
+
+    process.env["BUILD_BUILDID"] = "2";
+
+    const tp: string = path.join(__dirname, "setup.js");
+    const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+    // when
+    tr.run();
+
+    // then
+    expect(tr.succeeded).to.equals(true, "should have succeeded");
+    expect(tr.warningIssues.length).to.equals(0, "should have no warnings");
+    expect(tr.errorIssues.length).to.equals(0, "should have no errors");
+    expect(getVariable(tr, "Jira.Keys")).to.equal("JIE-1349");
     console.log(tr.stdout);
     done();
   });
